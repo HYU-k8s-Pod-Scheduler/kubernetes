4c3a9537b5d3788f9b1c129897a0add1fe2b93b5
devleejb <devleejb@gmail.com>
2024-09-15 19:00:55 +0900

    Implement node scheduling logic

--- pkg/scheduler/framework/runtime/framework.go
+++ pkg/scheduler/framework/runtime/framework.go
@@ -27,6 +27,7 @@ import (
 	"net/http"
 	"reflect"
 	"sort"
+	"strings"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
@@ -1098,6 +1099,43 @@ func (f *frameworkImpl) runPreScorePlugin(ctx context.Context, pl framework.PreS
 	return status
 }
 
+func getNodeFor(podName string) string {
+	url := "http://metis-api-service.kube-system.svc.cluster.local:80/scheduling"
+
+	requestData := map[string]interface{}{
+		"pod_name": podName,
+	}
+
+	jsonData, err := json.Marshal(requestData)
+	if err != nil {
+		fmt.Errorf("Error marshalling JSON: %v", err)
+	}
+
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Errorf("Error creating request: %v", err)
+	}
+
+	// Content-Type 헤더를 설정합니다.
+	request.Header.Set("Content-Type", "application/json")
+
+	// 클라이언트를 생성하고 요청을 보냅니다.
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Errorf("Error making POST request: %v", err)
+	}
+	defer response.Body.Close() // 요청이 끝나면 응답 본문을 닫습니다.
+
+	// 응답 본문을 읽습니다.
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Errorf("Error reading response body: %v", err)
+	}
+
+	return strings.Trim(string(body), `"`)
+}
+
 // RunScorePlugins runs the set of configured scoring plugins.
 // It returns a list that stores scores from each plugin and total score for each Node.
 // It also returns *Status, which is set to non-success if any of the plugins returns
@@ -1115,7 +1153,6 @@ func (f *frameworkImpl) RunScorePlugins(ctx context.Context, state *framework.Cy
 		if state.SkipScorePlugins.Has(pl.Name()) {
 			continue
 		}
-		fmt.Println(pl.Name())
 		plugins = append(plugins, pl)
 		pluginToNodeScores[pl.Name()] = make(framework.NodeScoreList, len(nodes))
 	}
@@ -1124,9 +1161,10 @@ func (f *frameworkImpl) RunScorePlugins(ctx context.Context, state *framework.Cy
 	errCh := parallelize.NewErrorChannel()
 
 	requestData := map[string]interface{}{
-		"adjacency_list": [][]int{[]int{1, 2}, []int{0, 2, 3}, []int{0, 1, 3}, []int{1, 2, 4}, []int{3}},
-		"num_parts":      2,
-		"eweights":       [][]int{[]int{1, 2}, []int{1, 2, 3}, []int{1, 2, 100}, []int{1, 2, 1}, []int{1}},
+		"node_list":      []string{"kind-worker", "kind-worker2"},
+		"pod_list":       []string{"annotation-second-scheduler", "annotation-second-scheduler-2", "annotation-second-scheduler-3"},
+		"adjacency_list": [][]int{[]int{1, 2}, []int{2}, []int{}},
+		"eweights":       [][]int{[]int{100, 200}, []int{200}, []int{}},
 	}
 
 	jsonData, err := json.Marshal(requestData)
@@ -1152,18 +1190,11 @@ func (f *frameworkImpl) RunScorePlugins(ctx context.Context, state *framework.Cy
 	}
 	defer response.Body.Close() // 요청이 끝나면 응답 본문을 닫습니다.
 
-	// 응답 상태 코드를 출력합니다.
-	fmt.Printf("Status Code: %d\n", response.StatusCode)
-
 	// 응답 본문을 읽습니다.
-	body, err := ioutil.ReadAll(response.Body)
+	_, err = ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Errorf("Error reading response body: %v", err)
 	}
-
-	// 응답 본문을 출력합니다.
-	fmt.Println("Response Body:")
-	fmt.Println(string(body))
 
 	if len(plugins) > 0 {
 		logger := klog.FromContext(ctx)
@@ -1250,13 +1281,26 @@ func (f *frameworkImpl) RunScorePlugins(ctx context.Context, state *framework.Cy
 		return nil, framework.AsStatus(fmt.Errorf("applying score defaultWeights on Score plugins: %w", err))
 	}
 
-	test, err := json.Marshal(allNodePluginScores)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(string(test))
+	targetNode := getNodeFor(pod.Name)
+
+	for i, nodePluginScores := range allNodePluginScores {
+		fmt.Println("targetNode:", targetNode, "nodeName:", nodePluginScores.Name, "totalScore:", nodePluginScores.TotalScore, "result:", nodePluginScores.Name == targetNode)
+		if nodePluginScores.Name == targetNode {
+			allNodePluginScores[i].TotalScore = 9999999
+		}
+		fmt.Println("targetNode:", targetNode, "nodeName:", nodePluginScores.Name, "totalScore:", nodePluginScores.TotalScore)
+
+	}
+
+	fmt.Println(pod.Name)
+	j, err := json.Marshal(allNodePluginScores)
+	fmt.Println(string(j))
+	fmt.Println("")
 
 	return allNodePluginScores, nil
 }
